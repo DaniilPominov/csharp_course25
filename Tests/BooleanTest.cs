7765d60e58c1d73daee85b2b348f2c180f4cfb7e
DaniilPominov <d.pominov2016@yandex.ru>
2025-04-10 17:39:41 +0700

    broken commit

--- Tests/BooleanTest.cs
+++ Tests/BooleanTest.cs
@@ -14,4 +14,12 @@ public class BooleanTest:BaseMathTest
         bob.Where((a)=> a==A);
         Assert.IsTrue(_math.CompareTriple(A,B,C));
     }
+     [TestMethod]
+    public void TestMethod2()
+    {
+        int A = 1,B = 2, C = 3;
+        var bob = new int[]{1,2,3};
+        bob.Where((a)=> a==A);
+        Assert.IsTrue(_math.CompareDouble(A,B,C));
+    }
 }
