c0f0d4de7615723bd79caeb0304b32ad44b44464
DaniilPominov <d.pominov2016@yandex.ru>
2025-04-10 19:40:03 +0700

    https://www.codewars.com/kata/52e88b39ffb6ac53a400022e/train/csharp

---
+++ KataTests/KataTests.cs
@@ -0,0 +1,21 @@
+﻿using csharp_course;
+using NUnit.Framework;
+using System;
+namespace KataTests;
+
+public class KataTests
+{
+
+
+  [TestFixture]
+  public class Sample_Test
+  {
+    [Test, Description("Sample Tests")]
+    public void Test()
+    {
+      Assert.That(Kata.UInt32ToIP(2154959208), Is.EqualTo("128.114.17.104"));
+      Assert.That(Kata.UInt32ToIP(0), Is.EqualTo("0.0.0.0"));
+      Assert.That(Kata.UInt32ToIP(2149583361), Is.EqualTo("128.32.10.1"));
+    }
+  }
+}
--- csharp_course/Kata.cs
+++ csharp_course/Kata.cs
@@ -1,8 +1,35 @@
 using System.Collections.Generic;
 using System;
+using System.Text;
+using System.Globalization;
+using System.Runtime.InteropServices;
 namespace csharp_course;
 public class Kata
 {
+     public static string UInt32ToIP(uint ip)
+  {
+    if(ip>0)
+    {
+        var binFormat = Convert.ToString(ip,2);
+        StringBuilder binBuilder = new();
+        for(int j = 32-binFormat.Length;j>0;j--){
+            binBuilder.Append("0");
+        }
+        binFormat = binBuilder.Append(binFormat).ToString();
+        string[] parts = new string[4];
+        for(int i=0; i<binFormat.Length;i+=8){
+            parts[i/8]=binFormat.Substring(i,8);
+        }
+        StringBuilder ipBuilder = new();
+        foreach(var numb in parts){
+            ipBuilder.Append(Convert.ToUInt32(numb,2));
+            ipBuilder.Append(".");
+        }
+        var res = ipBuilder.ToString().Trim('.');
+        return res;
+    }
+    return "0.0.0.0";
+  }
   public static string FirstNonRepeatingLetter(string s)
   {
     Dictionary<string, int> lettersInd = new();
